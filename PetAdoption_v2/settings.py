a83f7eb041dec0737e0b5dc7fef4f8b538173dad
MrTuzzO <khirulislam5750@gmail.com>
2025-01-31 01:29:39 +0600

    first commit

--- PetAdoption_v2/settings.py
+++ PetAdoption_v2/settings.py
@@ -34,6 +34,14 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1',
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://127.0.0.1:5500',
+    'http://127.0.0.1',
+    'https://mrtuzzo.github.io',
+)
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
