544682c84bf0cd1f6eab7b0556da15b7e8ddf95d
MrTuzzO <khirulislam5750@gmail.com>
2025-01-31 23:15:40 +0600

    bug fixed

--- PetAdoption_v2/settings.py
+++ PetAdoption_v2/settings.py
@@ -50,6 +50,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     'https://mrtuzzo.github.io',
+    'https://loveapet.netlify.app',
 ]
 
 # Application definition
