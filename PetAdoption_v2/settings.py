b04a6ab91380ee8e99f923ed673a004a648acfd9
MrTuzzO <khirulislam5750@gmail.com>
2025-01-31 23:10:25 +0600

    origin added

--- PetAdoption_v2/settings.py
+++ PetAdoption_v2/settings.py
@@ -50,7 +50,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     'https://mrtuzzo.github.io',
-    'https://loveapet.netlify.app/',
 ]
 
 # Application definition
