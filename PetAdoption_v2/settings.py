f6ba83d530d631b7e4caa05fc383196489e76bd9
MrTuzzO <khirulislam5750@gmail.com>
2025-01-31 16:46:32 +0600

    cloud removed

--- PetAdoption_v2/settings.py
+++ PetAdoption_v2/settings.py
@@ -133,23 +133,23 @@ WSGI_APPLICATION = 'PetAdoption_v2.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.idzjfbqinmomkwvnuzwt',
-        'PASSWORD': 'bQjYdtCZ65IyM7PI',
-        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
-        'PORT': '6543'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.idzjfbqinmomkwvnuzwt',
+#         'PASSWORD': 'bQjYdtCZ65IyM7PI',
+#         'HOST': 'aws-0-us-west-1.pooler.supabase.com',
+#         'PORT': '6543',
+#     }
+# }
 
 
 # Password validation
